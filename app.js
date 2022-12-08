0c4fd9fa2c04720218620c550f77d8f9956ea0cf
gnicolaslan <gabrielnicolas.dg@gmail.com>
2022-12-08 18:42:24 -0300

    nuevos estilos

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath))
 
 app.listen(3000, () => {
-    console.log("Servidor corriendo con exito!")
+    console.log("Servidor corriendo con exito!(Puerto 3000)")
 })
 
 app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')))
